a48c21102121b51fae0470667edbea8bdf1b2bfc
Sergey <sergey.bushnyak@sigrlami.eu>
2019-08-01 05:10:06 +0300

    small correction

--- aws.nix
+++ aws.nix
@@ -1,22 +1,22 @@
 let
   ## -- Global system options ---------------------------------------
-  awsProfile     = "default";                 # https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html
-  awsRegion      = "eu-central-1";            # https://docs.aws.amazon.com/general/latest/gr/rande.html
-  awsAccessKeyId = "AKIA5UA2KBIJSFJQYMPQ";
-  awsEC2type     = "t3.nano";                 # https://aws.amazon.com/ec2/instance-types/
+  profile     = "default";                 # https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html
+  region      = "eu-central-1";            # https://docs.aws.amazon.com/general/latest/gr/rande.html
+  accessKeyId = "AKIA5UA2KBIJSFJQYMPQ";
+  awsEC2type  = "t3.nano";                 # https://aws.amazon.com/ec2/instance-types/
 
 in {
 
   production = { resources, ... }: {
     deployment.targetEnv        = "ec2";
-    deployment.ec2.accessKeyId  = awsAccessKeyId;
-    deployment.ec2.region       = awsRegion;
+    deployment.ec2.accessKeyId  = accessKeyId;
+    deployment.ec2.region       = region;
     deployment.ec2.instanceType = awsEC2type;
     deployment.ec2.keyPair      = resources.ec2KeyPairs.minimal-keys;
     nixpkgs.localSystem.system  = "x86_64-linux";
   };
 
   resources.ec2KeyPairs.minimal-keys =
-    { inherit awsRegion awsAccessKeyId; };
+    { inherit region accessKeyId; };
 
 }
