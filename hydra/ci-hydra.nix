f9fd64c93b394d8399f2b2ed23929e773ced7b8f
Sergey <sergey.bushnyak@sigrlami.eu>
2019-08-02 16:31:30 +0300

    update hydra setup with proper ports and access tweaks

--- hydra/ci-hydra.nix
+++ hydra/ci-hydra.nix
@@ -3,7 +3,10 @@
   { networking.hostName = "hydra";
     networking.firewall.allowedTCPPorts =
       [ ## config.services.hydra.port 25
-        22 25 80 443
+        22   #
+        25
+        8080 # Hydra web UI
+        5000 # nix-serve daemon (Nix cache)
       ];
 
     services.hydra =
@@ -12,12 +15,20 @@
       minimumDiskFreeEvaluator = 10; # GiB
       hydraURL = "hydra.localhost.com";
       notificationSender = "admin@domain.com";
+      services.hydra.port = 8080;
+      services.hydra.logo = ./custom-logo.png;
     };
+
     programs.ssh.knownHosts =
       [{ hostNames = [ "github.com" ];
          publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
        }
       ];
+    # nix-serve - Nix cache daemon
+    services.nix-serve.enable = true;
+
+    # for remote access to the machine
+    services.openssh.enable = true;
 
     nix =
     { useSandbox   = true;
